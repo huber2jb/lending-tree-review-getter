856accdfbafc130154824a64f3d9548bb4b01a7e
Jacob Huber <huber2jb@mail.uc.edu>
2021-08-04 20:10:25 -0400

    Added try except for now

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+
+# Intial URL retrival and response check
 response = requests.get("https://www.lendingtree.com/reviews/personal/first-midwest-bank/52903183")
 soup = BeautifulSoup(response.text, features='lxml')
+totalReviews = soup.select_one('b.hidden-xs').text[:-8]
+
 reviewText, reviewAuthor, reviewTitle, reviewRating, reviewDate = [],[],[],[],[]
 reviewLoanType, reviewType = [],[]
-totalReviews = soup.select_one('b.hidden-xs').text
-print(totalReviews.find('Reviews'))
-for review in soup.findAll('div', attrs={'class':'mainReviews'}):
-    reviewTitle.append(review.find('p', attrs={'class':'reviewTitle'}).text.strip())
-    reviewText.append(review.find('p', attrs={'class':'reviewText'}).text.strip())
-    reviewAuthor.append(review.find('p', attrs={'class':'consumerName'}).text.strip()[:20].strip())
-    reviewRating.append(review.find('div', attrs={'class':'numRec'}).text.strip()[1:7])
-    reviewDate.append(review.find('p', attrs={'class':'consumerReviewDate'}).text.strip()[12:])
-    reviewLoanType.append(review.find('div', attrs={'class':'loanType'}).text.strip())
+for i in range(1,101):
+    try:
+        response = requests.get("https://www.lendingtree.com/reviews/personal/first-midwest-bank/52903183?pid="+str(i))
+        soup = BeautifulSoup(response.text, features='lxml')
+        for review in soup.findAll('div', attrs={'class':'mainReviews'}):
+            reviewTitle.append(review.find('p', attrs={'class':'reviewTitle'}).text.strip())
+            reviewText.append(review.find('p', attrs={'class':'reviewText'}).text.strip())
+            reviewAuthor.append(review.find('p', attrs={'class':'consumerName'}).text.strip()[:20].strip())
+            reviewRating.append(review.find('div', attrs={'class':'numRec'}).text.strip()[1:7])
+            reviewDate.append(review.find('p', attrs={'class':'consumerReviewDate'}).text.strip()[12:])
+            reviewLoanType.append(review.find('div', attrs={'class':'loanType'}).text.strip())
+    except Exception as e:
+        print(e)
 
-dataJson = {'Reviews': [{'reviewTitle':title, 
+dataJson = {'Reviews': [{'reviewNumber':number+1,
+                        'reviewTitle':title, 
                         'reviewText':text,
                         'reviewAuthor':author,
                         'reviewRating':rating,
                         'reviewDate':date,
-                        'reviewLoanType':loanType} for title, text, author, rating, date, loanType in zip(reviewTitle, reviewText, reviewAuthor, reviewRating, reviewDate, reviewLoanType)]}
+                        'reviewLoanType':loanType} for number, (title, text, author, rating, date, loanType) in enumerate(zip(reviewTitle, reviewText, reviewAuthor, reviewRating, reviewDate, reviewLoanType))]}
 
-# print(json.dumps(dataJson, indent=4))
+print(json.dumps(dataJson, indent=4))
